b8b843ecc8beca5f59dd2820c6e3337ca8d71dc8
Bohdan <mrgumor2017@gmail.com>
2024-10-18 21:05:29 +0300

    Add project files.

---
+++ ClientAppChat/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientAppChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverEndPoint;
+        UdpClient udpClient;
+        ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = messages;
+            udpClient = new UdpClient();
+            string address = ConfigurationManager.AppSettings["serverAddres"]!;
+            short port = short.Parse(ConfigurationManager.AppSettings["serverPort"]!);
+
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        }
+
+        private void Leave_Button_click(object sender, RoutedEventArgs e)
+        {
+            string message = "$<leave>";
+            SendMessage(message);
+        }
+
+        private void Join_Button_click(object sender, RoutedEventArgs e)
+        {
+            string nickname = Nickname.Text;
+            if (string.IsNullOrWhiteSpace(nickname))
+            {
+                MessageBox.Show("Введіть нікнейм.");
+            }
+            else
+            {
+                string message = $"$<join>{nickname}";
+                SendMessage(message);
+                Listen();
+            }
+            
+        }
+
+        private void Send_Button_click(object sender, RoutedEventArgs e)
+        {
+           
+            string message = Msg_text.Text;
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                SendMessage(message);
+            }
+            else
+            {
+                MessageBox.Show("Повідомлення не може бути порожнім.");
+            }
+            
+            Msg_text.Clear();
+            
+        }
+
+        private async void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await udpClient.SendAsync(data, serverEndPoint);
+        }
+
+        private async void Listen()
+        {
+            while (true)
+            {
+                var data = await udpClient.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(data.Buffer);
+                messages.Add(new MessageInfo(message, DateTime.Now));
+            }
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    class ServerChat
+    {
+        const short port = 4040;
+        const string JOIN_CMD = "$<join>";
+        UdpClient server;
+        List<IPEndPoint> members;
+        IPEndPoint clientendpoint = null;
+        Dictionary<IPEndPoint, string> membersWithNicknames = new Dictionary<IPEndPoint, string>();
+
+        public ServerChat()
+        {
+            server = new UdpClient(port);
+            members = new List<IPEndPoint>();
+        }
+        private void AddMember(string nickname)
+        {
+            if (!membersWithNicknames.ContainsKey(clientendpoint))
+            {
+                membersWithNicknames.Add(clientendpoint, nickname);
+                Console.WriteLine($"Member added: {nickname} from {clientendpoint}");
+            }
+        }
+        private void SendToAll(byte[] data)
+        {
+            foreach (IPEndPoint p in membersWithNicknames.Keys)
+            {
+                server.SendAsync(data, data.Length, p);
+            }
+        }
+
+        private void RemoveMember()
+        {
+            if (members.Contains(clientendpoint))
+            {
+                members.Remove(clientendpoint);
+                Console.WriteLine("Member removed: " + clientendpoint);
+            }
+        }
+        public void Start()
+        {
+            Console.WriteLine("Server started...");
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientendpoint);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine($"{message} from {clientendpoint}. Date: {DateTime.Now}");
+
+                if (message.StartsWith("$<join>"))
+                {
+                    string nickname = message.Substring(7); 
+                    AddMember(nickname);
+                }
+                else if (message == "$<leave>")
+                {
+                    RemoveMember();
+                }
+                else
+                {
+                    if (membersWithNicknames.TryGetValue(clientendpoint, out string nickname))
+                    {
+                        string messageWithNick = $"{nickname}: {message}";
+                        SendToAll(Encoding.UTF8.GetBytes(messageWithNick));
+                    }
+                }
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerChat serverChat = new ServerChat();
+            serverChat.Start();
+            
+        }
+    }
+}
